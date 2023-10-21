07300854319912d69d8789523e3004b5893ccade
SSBakh07 <ssbakh07@gmail.com>
2023-10-21 03:55:29 +0330

    added extra view

---
+++ readycheck/CheckView.py
@@ -0,0 +1,37 @@
+import discord
+
+class CheckView(discord.ui.View):
+    def __init__(
+        self,
+        author: Optional[discord.abc.User] = None,
+        *,
+        timeout: float = 60.0,
+    ):
+        if timeout is None:
+            raise TypeError("This view should not be used as a persistent view.")
+        super().__init__(timeout=timeout)
+
+        self.message: Optional[discord.Message] = None
+        self.interacted_users = []
+
+    async def on_timeout(self):
+        if self.message is None:
+            # we can't do anything here if message is none
+            return
+            
+        try:
+            await self.message.edit(view=view)
+        except discord.HTTPException:
+            # message could no longer be there or we may not be able to edit it anymore
+            pass
+
+
+    @discord.ui.button(label=_("Ready"), style=discord.ButtonStyle.green)
+    async def confirm_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.interacted_users.append(interaction.user.id)
+        self.stop()
+        # respond to the interaction so the user does not see "interaction failed".
+        await interaction.response.defer()
+        # call `on_timeout` explicitly here since it's not called when `stop()` is called.
+        await self.on_timeout()
+    
--- readycheck/readycheck.py
+++ readycheck/readycheck.py
@@ -8,13 +8,24 @@ from redbot.core.utils.chat_formatting import inline, humanize_list
 from redbot.core.utils.predicates import MessagePredicate
 from redbot.core.utils.embed import randomize_colour
 from redbot.core.utils.menus import menu, DEFAULT_CONTROLS
+from redbot.core.utils.views import ConfirmView
+import asyncio
+from CheckView import CheckView
+# https://stackoverflow.com/questions/62507316/executing-a-function-after-a-certain-period-of-time (asyncio)
 
 class Buttons(discord.ui.View):
     def __init__(self, *, timeout=180):
         super().__init__(timeout=timeout)
-    @discord.ui.button(label="Button",style=discord.ButtonStyle.gray)
-    async def gray_button(self,button:discord.ui.Button,interaction:discord.Interaction):
+
+    @discord.ui.button(label="Button",style=discord.ButtonStyle.green)
+    async def green_button(self, button:discord.ui.Button, interaction:discord.Interaction):
         await interaction.response.edit_message(content=f"This is an edited button response!")
+        self.stop()
+        # respond to the interaction so the user does not see "interaction failed".
+        await interaction.response.defer()
+        # call `on_timeout` explicitly here since it's not called when `stop()` is called.
+        await self.on_timeout()
+
 
 
 class ReadyCheck(commands.Cog):
@@ -44,4 +55,13 @@ class ReadyCheck(commands.Cog):
         """
             Publish test message
         """
-        await ctx.send("Test message!", view=Buttons())
+        # msg = await ctx.send("Test message!", view=Buttons())
+        view = CheckView()
+
+        view.message = await ctx.send("Are you sure you about that?", view=view)
+        await view.wait()
+
+        final_msg = " ".join([str(id) for id in view.interacted_users])
+        await ctx.send(final_msg)
+
+        
